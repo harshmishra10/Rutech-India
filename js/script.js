f73b48687fd6a8f41a09e372e59ffb9115bd0014
harshmishra10 <wgpthm@gmail.com>
2025-12-30 01:56:15 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+    reveals.forEach(el => {
+        const top = el.getBoundingClientRect().top;
+        const height = window.innerHeight;
+        if (top < height - 100) {
+            el.classList.add("active");
+        }
+    });
+});
