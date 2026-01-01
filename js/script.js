14a638e24da669fcdbd544956261c9bbe2342be8
harshmishra10 <wgpthm@gmail.com>
2026-01-01 16:06:35 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -42,4 +42,26 @@ if (searchInput) {
 }
 
 });
+/* =====================
+   THEME TOGGLE
+===================== */
+
+const toggleBtn = document.getElementById("themeToggle");
+const root = document.documentElement;
+
+// Load saved theme
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme) {
+  root.setAttribute("data-theme", savedTheme);
+  toggleBtn.textContent = savedTheme === "light" ? "🌞" : "🌙";
+}
+
+// Toggle theme
+toggleBtn.addEventListener("click", () => {
+  const currentTheme = root.getAttribute("data-theme") === "light" ? "dark" : "light";
+  root.setAttribute("data-theme", currentTheme);
+  localStorage.setItem("theme", currentTheme);
+  toggleBtn.textContent = currentTheme === "light" ? "🌞" : "🌙";
+});
+
 
